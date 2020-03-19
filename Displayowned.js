b6b5f98b7cbfbcc85e891c0a9eb8e91d0fd2ce8b
Nathaniel Redmon <52013906+nonstopper0@users.noreply.github.com>
2020-03-18 18:05:28 -0600

    Formatting data on the screen

--- Displayowned.js
+++ Displayowned.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, ScrollView, ActivityIndicator, Dimensions} from 'react-native'
+import { View, ScrollView, ActivityIndicator, Dimensions, ActionSheetIOS, ActivityIndicatorComponent, Text} from 'react-native'
 
 export default class DisplayOwned extends React.Component {
     constructor() {
@@ -20,10 +20,40 @@ export default class DisplayOwned extends React.Component {
                 this.setState({
                     data: parsedResponse.data,
                     isLoaded: true,
-                }) 
-                console.log(parsedResponse.data)              
+                })             
             } else {
                 console.log('no data')
+            }
+        } catch(err) {
+            console.log(err)
+        }
+    }
+    sell = async(id) => {
+        try {
+            const data = {
+                id: id
+            }
+            const response = await fetch(`https://nomistockexpress.herokuapp.com/stock/sell`, {
+                method: 'POST',
+                body: JSON.stringify(data),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            const parsedResponse = await response.json()
+            if (parsedResponse.status === 200) {
+                const newState = this.state.data.filter((data) => {
+                    return data.id !== id
+                })
+                this.props.add('add', parsedResponse.money)
+                this.setState({
+                    data: newState,
+                    sellStockMessage: parsedResponse.data
+                })
+            } else {
+                this.setState({
+                    sellStockMessage: parsedResponse.data
+                })
             }
         } catch(err) {
             console.log(err)
@@ -51,6 +81,17 @@ export default class DisplayOwned extends React.Component {
                             }}
                         >
                         </View>
+                        { this.state.isLoaded ? 
+                        this.state.data.map((stock) => {
+                            return (
+                                <View key={stock.id} style={{flex: 1, alignItems: 'center', width: Dimensions.get("window").width, backgroundColor: 'rgb(38,38,38)', borderRadius: 20, marginBottom: 12}}>
+                                    <Text>{stock.stock_name}</Text>
+                                </View>
+                            )
+                        })
+                        :
+                        null
+                        }
                     </ScrollView>
                 </View>
             </View>
